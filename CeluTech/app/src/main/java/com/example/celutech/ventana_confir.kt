2b7df0b2236fff3c0abad41b695537a958e78213
ftaya <ftayala89@gmail.com>
2023-06-09 08:48:35 -0600

    Modifique alerta de confirmacion y menu

---
+++ CeluTech/app/src/main/java/com/example/celutech/ventana_confir.kt
@@ -0,0 +1,11 @@
+package com.example.celutech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ventana_confir : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ventana_confir)
+    }
+}
